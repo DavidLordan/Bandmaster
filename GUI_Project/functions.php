9c9133e0688aefd01abb74684b930d9c2fbbc77f
David Lordan <DaveLordan@Davids-MacBook-Pro.local>
2015-02-24 13:41:43 -0500

    Inital commit, added all GUI project files.

---
+++ GUI_Project/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+set_include_path('ChromePHP.php');
+
+$file = file_get_contents('JSON/GB_songs.json', true);
+$data = json_decode($file, true);
+unset($file);
+
+array_splice($data, $_POST['index'], 1);
+
+$result = json_encode($data);
+file_put_contents('JSON/GB_songs.json', $result);
+unset($result);
+
+$fileToRemove = 'uploads/' . (string)$_POST['filename'];
+unlink($fileToRemove);
+
+exit;

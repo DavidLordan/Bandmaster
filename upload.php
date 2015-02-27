d9029f656c6a4bb347f372e6a8a5bdde0d136526
paulkarcher <paulkarcher@users.noreply.github.com>
2015-02-27 01:34:11 -0500

    Added comments and updated the error detection
    We can now detect if a file that's being uploaded exceeds the file size limit set by php.ini. However, for now there is no consequence in place if a file does in fact exceed that limit.

--- upload.php
+++ upload.php
@@ -1,30 +1,38 @@
 <?php
 
+if (isset($_FILES['upl'])) {
+  /* If the file size limit specified in php.ini is exceeded.  */
+  if ($_FILES['upl']['error'] == 1) {
+    /* insert consequence here */
+  }
 
-if (isset($_FILES['upl']) && $_FILES['upl']['error'] == 0) {
-
+  /* If the file is not null and there is no error, the file uploaded with success. */
+  if ($_FILES['upl']['error'] == 0) {
+    
     $extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
 
     if (move_uploaded_file($_FILES['upl']['tmp_name'], 'uploads/' . $_FILES['upl']['name'])) {
-        echo '{"status":"success"}';
+      echo '{"status":"success"}';
 
 
-        $file = file_get_contents('JSON/songs.json', true);
-        $data = json_decode($file, true);
-        unset($file);
+      $file = file_get_contents('JSON/songs.json', true);
+      $data = json_decode($file, true);
+      unset($file);
 
-        $basename = pathinfo($_FILES['upl']['name'], PATHINFO_BASENAME);
-        //$data[0]["name"] = $basename;
+      $basename = pathinfo($_FILES['upl']['name'], PATHINFO_BASENAME);
+      //$data[0]["name"] = $basename;
 
-        $data[] = array('name' => $basename);
+      $data[] = array('name' => $basename);
 
-        $result = json_encode($data);
-        file_put_contents('JSON/songs.json', $result);
-        unset($result);
+      $result = json_encode($data);
+      file_put_contents('JSON/songs.json', $result);
+      unset($result);
 
-        exit;
+      exit;
     }
+  }
 }
+
 
 echo '{"status":"error"}';
 exit;

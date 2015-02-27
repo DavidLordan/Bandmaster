d5ea8094eb11a59dda11611640b87639d83ff3c2
David Lordan <DavidLordan@users.noreply.github.com>
2015-02-26 22:34:17 -0500

    Update functions.php
    Updated songs.json path.

--- functions.php
+++ functions.php
@@ -2,14 +2,14 @@
 
 set_include_path('ChromePHP.php');
 
-$file = file_get_contents('JSON/GB_songs.json', true);
+$file = file_get_contents('JSON/songs.json', true);
 $data = json_decode($file, true);
 unset($file);
 
 array_splice($data, $_POST['index'], 1);
 
 $result = json_encode($data);
-file_put_contents('JSON/GB_songs.json', $result);
+file_put_contents('JSON/songs.json', $result);
 unset($result);
 
 $fileToRemove = 'uploads/' . (string)$_POST['filename'];
